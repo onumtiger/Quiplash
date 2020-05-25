46ae37914f386714964d0f2080453cb2e39b55df
lalekaya <l.kaya@campus.lmu.de>
2020-05-25 16:52:16 +0200

    [Feature] App Main Structure initial

--- app/src/main/java/com/example/quiplash/MainActivity.kt
+++ app/src/main/java/com/example/quiplash/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.quiplash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSignIn = findViewById<Button>(R.id.sign_in)
+        val btnSignUp = findViewById<Button>(R.id.sign_up)
+        val btnStart = findViewById<Button>(R.id.start)
+
+        btnSignIn.setOnClickListener() {
+            val intent = Intent(this, SignIn::class.java);
+            startActivity(intent);
+        }
+
+        btnSignUp.setOnClickListener() {
+            val intent = Intent(this, SignUp::class.java);
+            startActivity(intent);
+        }
+
+        btnStart.setOnClickListener() {
+            val intent = Intent(this, HomeScreen::class.java);
+            startActivity(intent);
+        }
     }
 }
