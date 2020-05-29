058e10d328e73ca8d679674f879c508ecad9074c
lalekaya <l.kaya@campus.lmu.de>
2020-05-29 19:21:59 +0200

    [Feature] Back arrows functionality, join game view

--- app/src/main/java/com/example/quiplash/New_GameActivity.kt
+++ app/src/main/java/com/example/quiplash/New_GameActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.widget.AppCompatImageButton
 
 class New_GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_game)
-        val btnHome = findViewById<Button>(R.id.btnHome)
 
-        btnHome.setOnClickListener() {
-            val intent = Intent(this, LandingActivity::class.java);
-            startActivity(intent);
+        val btnBack = findViewById<AppCompatImageButton>(R.id.new_game_go_back_arrow)
+
+        btnBack.setOnClickListener() {
+            super.onBackPressed();
         }
     }
 }
--- app/src/main/java/com/example/quiplash/Profile_RegisteredActivity.kt
+++ app/src/main/java/com/example/quiplash/Profile_RegisteredActivity.kt
@@ -1,20 +1,28 @@
 package com.example.quiplash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.widget.AppCompatImageButton
 
 class Profile_RegisteredActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_registered)
         val btnHome = findViewById<Button>(R.id.btnHome)
+        val btnBack = findViewById<AppCompatImageButton>(R.id.profile_game_go_back_arrow)
 
         btnHome.setOnClickListener() {
             val intent = Intent(this, LandingActivity::class.java);
             startActivity(intent);
+        }
+
+        btnBack.setOnClickListener() {
+            super.onBackPressed();
         }
     }
 }
