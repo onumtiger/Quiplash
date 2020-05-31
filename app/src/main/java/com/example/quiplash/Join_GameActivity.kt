82207fa281ec42e15d42123836d3245555823db5
lalekaya <l.kaya@campus.lmu.de>
2020-05-31 22:58:40 +0200

    [Feature] ListViews for host waiting & join game

--- app/src/main/java/com/example/quiplash/Join_GameActivity.kt
+++ app/src/main/java/com/example/quiplash/Join_GameActivity.kt
@@ -2,10 +2,13 @@ package com.example.quiplash
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
+
 
 class Join_GameActivity : AppCompatActivity() {
 
@@ -25,6 +28,20 @@ class Join_GameActivity : AppCompatActivity() {
         btnBack.setOnClickListener() {
             super.onBackPressed();
         }
+
+        val activeGamesList = findViewById<ListView>(R.id.active_games_list)
+        val activeGamesArray = arrayOfNulls<String>(5)
+
+        for (i in 0 until activeGamesArray.size) {
+            activeGamesArray[i] = "Active Game $i"
+        }
+
+        val adapter = ArrayAdapter<String>(
+            this, R.layout.active_game_list_item,
+            R.id.active_player, activeGamesArray
+        )
+
+        activeGamesList.adapter = adapter
 
 
     }
