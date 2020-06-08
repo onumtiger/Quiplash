5be59eefcf45ba56c3debb0e7299590d96e7bf08
onum <>
2020-06-08 08:24:18 +0200

    store image in firebase

--- app/src/main/java/com/example/quiplash/Edit_ProfileActivity.kt
+++ app/src/main/java/com/example/quiplash/Edit_ProfileActivity.kt
@@ -3,20 +3,26 @@ package com.example.quiplash
 import android.R.attr
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import com.example.quiplash.DBMethods.DBCalls.Companion.editUser
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.IOException
 import java.io.InputStream
+import java.util.*
 
 
 class Edit_ProfileActivity : AppCompatActivity() {
@@ -25,6 +31,12 @@ class Edit_ProfileActivity : AppCompatActivity() {
     private var authListener: FirebaseAuth.AuthStateListener? = null
     private val GALLERY_REQUEST_CODE = 100
     private val CAMERA_REQUEST_CODE = 200
+    private val PICK_IMAGE_REQUEST = 71
+    private lateinit var filePath: Uri
+
+    //Firebase
+    private var storage: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
 
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +44,9 @@ class Edit_ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.acitvity_edit_profile)
 
         auth = FirebaseAuth.getInstance()
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage!!.getReference();
 
         val btnBack = findViewById<AppCompatImageButton>(R.id.profile_game_go_back_arrow)
         val btnSave = findViewById<Button>(R.id.btnSave)
@@ -53,7 +68,8 @@ class Edit_ProfileActivity : AppCompatActivity() {
 
         btnEditPicture.setOnClickListener(){
             // pickFromGallery()
-            pickFromCamera()
+            // pickFromCamera()
+            chooseImage()
        }
 
         btnSave.setOnClickListener() {
@@ -72,6 +88,7 @@ class Edit_ProfileActivity : AppCompatActivity() {
             // TO DO: Save user data to firebase
             setUserInfo(username, email, password)
 
+            uploadImage()
             val intent = Intent(this, Profile_RegisteredActivity::class.java);
             startActivity(intent);
         }
@@ -80,6 +97,7 @@ class Edit_ProfileActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         var viewProfilePic: ImageView = findViewById(R.id.imageView)
+        // pick from gallery
         if (resultCode === Activity.RESULT_OK && requestCode === GALLERY_REQUEST_CODE) {
             try {
                 val selectedImage = data?.data
@@ -94,10 +112,24 @@ class Edit_ProfileActivity : AppCompatActivity() {
             }
         }
 
+        // pick from camera
         if (requestCode === CAMERA_REQUEST_CODE && resultCode === Activity.RESULT_OK) {
             val extras: Bundle? = data?.extras
             val imageBitmap = extras?.get("data") as Bitmap?
             viewProfilePic.setImageBitmap(imageBitmap)
+        }
+
+        // choose image
+        if (requestCode === PICK_IMAGE_REQUEST && resultCode === Activity.RESULT_OK && attr.data != null && data?.data != null
+        ) {
+            filePath = data?.data!!
+            try {
+                val bitmap =
+                    MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                viewProfilePic.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 
@@ -138,6 +170,42 @@ class Edit_ProfileActivity : AppCompatActivity() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (takePictureIntent.resolveActivity(packageManager) != null) {
             startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+        }
+    }
+
+    private fun chooseImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        val mimeTypes =
+            arrayOf("image/jpeg", "image/png")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+    private fun uploadImage() {
+        if (filePath != null) {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+            val ref =
+                storageReference!!.child("images/" + UUID.randomUUID().toString())
+            ref.putFile(filePath)
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(this@Edit_ProfileActivity, "Uploaded", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    progressDialog.dismiss()
+                    Toast.makeText(this@Edit_ProfileActivity, "Failed " + e.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .addOnProgressListener { taskSnapshot ->
+                    val progress =
+                        100.0 * taskSnapshot.bytesTransferred / taskSnapshot
+                            .totalByteCount
+                    progressDialog.setMessage("Uploaded " + progress.toInt() + "%")
+                }
         }
     }
 
