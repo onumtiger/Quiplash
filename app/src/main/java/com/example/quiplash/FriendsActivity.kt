d8f488dc9a81ec935241890269eb6dc88f734eb3
onum <>
2020-06-08 14:27:04 +0200

    Added add_friend button

--- app/src/main/java/com/example/quiplash/FriendsActivity.kt
+++ app/src/main/java/com/example/quiplash/FriendsActivity.kt
@@ -14,9 +14,22 @@ class FriendsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_friends)
 
         val btnBack = findViewById<AppCompatImageButton>(R.id.friends_go_back_arrow)
+        val btnFriend = findViewById<AppCompatImageButton>(R.id.add_friend_btn)
 
         btnBack.setOnClickListener() {
             super.onBackPressed();
+        }
+
+        btnFriend.setOnClickListener(){
+            val dialogFragment = Add_Player()
+            val ft = supportFragmentManager.beginTransaction()
+            val prev = supportFragmentManager.findFragmentByTag("add")
+            if (prev != null)
+            {
+                ft.remove(prev)
+            }
+            ft.addToBackStack(null)
+            dialogFragment.show(ft, "delete")
         }
 
 
