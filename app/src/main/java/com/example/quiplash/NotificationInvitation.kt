add18d22894dfa9d0afcf622b21dccb2373e09fa
onum <>
2020-06-26 12:23:15 +0200

    try to fix fetch game

--- app/src/main/java/com/example/quiplash/NotificationInvitation.kt
+++ app/src/main/java/com/example/quiplash/NotificationInvitation.kt
@@ -11,8 +11,11 @@ import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.util.*
@@ -21,9 +24,67 @@ import java.util.*
 class NotificationInvitation : FirebaseMessagingService()  {
 
     private val ADMIN_CHANNEL_ID = "admin_channel"
+    lateinit var db: CollectionReference
+    private val dbGamesPath = "games"
+
 
     @SuppressLint("ResourceAsColor")
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        db = FirebaseFirestore.getInstance().collection(dbGamesPath)
+        remoteMessage.data["gameID"]?.let {
+            db.document(it).get()
+                .addOnSuccessListener { documentSnapshot ->
+                    try {
+                        GameManager.game = documentSnapshot.toObject(Game::class.java)!!
+                    } finally {
+                        val intent = Intent(this, HostWaitingActivity::class.java)
+                        intent.putExtra("gameID", remoteMessage.data["gameID"])
+
+                        val notificationManager =
+                            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        //Setting up Notification channels for android O and above
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            setupChannels(notificationManager)
+                        }
+
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        val pendingIntent = PendingIntent.getActivity(
+                            applicationContext, 0, intent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        )
+
+                        /*val pendingIntent = PendingIntent.getBroadcast(
+                            applicationContext,
+                            intent,  // as stated in the comments, this flag is important!
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                        )*/
+
+                        val largeIcon = BitmapFactory.decodeResource(
+                            resources,
+                            R.drawable.ic_launcher_round
+                        )
+                        val notificationId = Random().nextInt(60000)
+                        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                        val notificationBuilder =
+                            NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                                .setSmallIcon(R.drawable.logo)
+                                .setLargeIcon(largeIcon)
+                                .setContentTitle(
+                                    remoteMessage.data["title"]
+                                )
+                                .setContentText(remoteMessage.data["message"])
+                                .setAutoCancel(true) //dismisses the notification on click
+                                .setSound(defaultSoundUri)
+                                .setContentIntent(pendingIntent)
+                        notificationManager.notify(
+                            notificationId,
+                            notificationBuilder.build()
+                        )
+                    }
+
+                }
+        }
+/*
         val intent = Intent(this, HostWaitingActivity::class.java)
         intent.putExtra("gameID", remoteMessage.data["gameID"])
         val notificationManager =
@@ -35,8 +96,8 @@ class NotificationInvitation : FirebaseMessagingService()  {
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
-            this, 0, intent,
-            PendingIntent.FLAG_CANCEL_CURRENT
+            applicationContext, 0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
         )
 
         /*val pendingIntent = PendingIntent.getBroadcast(
@@ -65,7 +126,7 @@ class NotificationInvitation : FirebaseMessagingService()  {
         notificationManager.notify(
             notificationId,
             notificationBuilder.build()
-        )
+        )*/
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
